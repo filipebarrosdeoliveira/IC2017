1277d5e9a8de2fbdd43998a0b8182b4bc00af4d8
Filipe Barros <barros7@icloud.com>
2017-03-22 16:02:42 -0300

    2.c

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+int area, base,altura;
+
+printf ("Entre com a base \n");
+scanf ("%d", & base);
+printf ("Entre com a altura \n");
+scanf ("%d", & altura);
+
+printf("%d \n", (area =  (base*altura)/2));
+
+return 0;
+}
