fa5df6a2ed454920111ac900b3a133358c37c6ec
Filipe Barros <barros7@icloud.com>
2017-04-06 09:29:26 -0300

    salario.c

---
+++ salario.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+	float salario;
+	scanf ("%f", & salario);
+
+	if ( salario <= 500 )
+		printf ("Receberá aumento \n Valor a ser recebido será de %f \n", salario = ((salario * 0.3) + salario));
+
+	else 
+		printf ("Não receberá aumento");
+
+return 0;
+
+}
