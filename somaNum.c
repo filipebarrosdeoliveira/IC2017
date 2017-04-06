d90458e4bd7e6c006cc1c2e3dec5b5a69ae004ec
Filipe Barros <barros7@icloud.com>
2017-04-06 09:30:50 -0300

    somaNum.c

---
+++ somaNum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+	int x,y,z;
+	scanf ("%d %d %d", &x, &y, &z);
+
+	if ( x + y + z >= 100) 
+		printf ("Numero maior que 100 \n");
+
+	else 
+		printf ("Número menor que 100 \n");
+		
+return 0;
+}
+	
+
