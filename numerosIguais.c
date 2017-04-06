5d6b7bdc69b029ca58bd6b916868bc0a9c417810
Filipe Barros <barros7@icloud.com>
2017-04-06 09:31:54 -0300

    numerosIguais.c

---
+++ numerosIguais.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+
+	int x,y;
+	scanf ("%d", & x);
+	scanf ("%d", & y);
+
+	if (x == y) 
+		printf ("São iguais");
+
+
+	else 
+		printf ("Não são iguais");
+
+		
+return 0;
+}
+
+
+
+
+
+
