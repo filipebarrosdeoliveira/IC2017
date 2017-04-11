734ab3253b865ddeec45fe8903d5a8e20ebe562e
Filipe Barros <barros7@icloud.com>
2017-04-11 09:30:52 -0300

    nomeProduto.c

---
+++ nomeProduto.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+	
+	int cod;
+	
+	scanf ("%d", & cod);
+
+	if ( cod == 1 ) 
+		printf ("Parafuso");
+	if ( cod == 2 ) 
+		printf ("Porca");
+	if ( cod == 3 )
+		printf ("Prego");
+	
+	else 
+        printf ("Diversos");
+				
+
+  return 0;
+}
+	
+	 
